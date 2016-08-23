8fef6e9b6ecc14849584238161598aee0abbf568
susan <susan.ra.evans@gmail.com>
2016-08-22 19:16:48 -0700

    setup instance variables, constructor, make_guess mehtod, and print_word_so_far method

--- player.rb
+++ player.rb
@@ -1,9 +1,38 @@
-class Player {
+class Player 
 
-	attr_accessor :name, :turn
+	attr_accessor :name, :turn, :guessed, :word_so_far
 
-	def initalize(name){
+	def initialize(name)
 		@name = name
 		@turn = 0
-	}
-}
+		@guessed = []
+		@word_so_far = []
+
+		for i in 0..4
+			@word_so_far.push("_")
+		end
+	end
+
+	def make_guess(indexes, letter)
+		if !(@guessed.include? letter)
+			@guessed.push(letter)
+			@turn+=1
+		else
+			puts "you already guessed that letter"
+		end
+
+		if indexes.length > 0
+			for i in 0..indexes.length-1
+				word_so_far[indexes[i]] = letter
+			end
+		end
+	end
+
+	def print_word_so_far
+		for i in word_so_far 
+			print (i + " ")
+		end
+
+		print "\n"
+	end
+end
